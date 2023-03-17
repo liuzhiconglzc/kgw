bdbbc0dd99b0660fef1380f294af6b3bb019705a
时倾 <11951820+liuzhicong0308@user.noreply.gitee.com>
2023-03-17 09:54:59 +0000

    更新
    Signed-off-by: 时倾 <11951820+liuzhicong0308@user.noreply.gitee.com>

--- src/view/pc/password/index.vue
+++ src/view/pc/password/index.vue
@@ -4,10 +4,10 @@
       <div class="title">请输入手机号及真实姓名重置密码</div>
       <van-form @submit="onSubmit" class="form_back">
         <van-field v-model="username" name="username" label-width="4em" type="tel" label="手机号" placeholder="请输入正确格式的手机号码"
-          :rules="telRules" />
+          :rules="telRules" style="margin-top: 10%;"/>
         <van-field v-model="nickname" name="nickname" label-width="4em" label="真实姓名" placeholder="请输入真实姓名"
-          :rules="[{ required: true}]" style="margin-top: 5%;"/>
-        <div style="margin: 16px;">
+          :rules="[{ required: true}]" />
+        <div style="margin: 16px;margin-top: 5%;margin-bottom: 10%;">
           <van-button class="register_btn" block :loading="loading" color="var(--themeColor)"
             native-type="submit">提交
           </van-button>
@@ -66,16 +66,17 @@
     position: fixed;
     left: 65%;
     width: 300px;
-    height: calc(100% - 60%);
+    margin-top: 5%;
+    height: calc(100% - 70%);
     // overflow-y: hidden;
     display: flex;
     flex-direction: column;
     background: white;
-    margin-top: 15px;
+    // margin-top: 15px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
     // background: linear-gradient(to bottom, #66c6a1, #f2f2f2 50%);
     .form_back {
-      margin-top: 5%;
+      margin-top: -5%;
       margin-left: 5%;
       width: 90%;
       // height: 250px;
@@ -88,11 +89,11 @@
       text-align: left;
     }
     .register_btn{
-      margin-top: 10%;
+      margin-top: -5%;
       border-radius: 5px;
       width: 80px;
       height: 30px;
-      margin-left: 30%;
+      margin-left: 32%;
     }
   }
   :deep(.van-nav-bar__title) {
