ad7607084d12bf12f84d8853557a7b0b9c5c0b75
时倾 <11951820+liuzhicong0308@user.noreply.gitee.com>
2023-03-19 03:35:43 +0000

    更新
    Signed-off-by: 时倾 <11951820+liuzhicong0308@user.noreply.gitee.com>

--- src/view/pc/password/index.vue
+++ src/view/pc/password/index.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="back">
-      <!-- <van-nav-bar title="重置密码" :border="false" left-arrow fixed @click-left="goBack" /> -->
-      <div class="title">请输入手机号及真实姓名重置密码</div>
-      <van-form @submit="onSubmit" class="form_back">
-        <van-field v-model="username" name="username" label-width="4em" type="tel" label="手机号" placeholder="请输入正确格式的手机号码"
-          :rules="telRules" style="margin-top: 10%;"/>
-        <van-field v-model="nickname" name="nickname" label-width="4em" label="真实姓名" placeholder="请输入真实姓名"
-          :rules="[{ required: true}]" />
-        <div style="margin: 16px;margin-top: 5%;margin-bottom: 10%;">
-          <van-button class="register_btn" block :loading="loading" color="var(--themeColor)"
-            native-type="submit">提交
-          </van-button>
-        </div>
-      </van-form>
+      <div class="login">
+        <!-- <div class="title">请输入手机号及真实姓名重置密码</div> -->
+        <p>PASSWORD</p>
+        <van-form @submit="onSubmit" class="form_back">
+          <van-field v-model="username" name="username" label-width="4em" type="tel" label="手机号" placeholder="请输入正确格式的手机号码"
+            :rules="telRules" style="margin-top: -5%;font-size: 18px;"/>
+          <van-field v-model="nickname" name="nickname" label-width="4em" label="真实姓名" placeholder="请输入真实姓名"
+            :rules="[{ required: true}]" style="margin-top: 5%;font-size: 28px;font-size: 18px;"/>
+          <div style="margin: 16px;margin-top: 5%;margin-bottom: 10%;">
+            <van-button class="register_btn" block :loading="loading" color="var(--themeColor)"
+              native-type="submit">提交
+            </van-button>
+          </div>
+        </van-form>
+      </div>
     </div>
   </template>
   
@@ -62,19 +64,33 @@
   
   <style lang="less" scoped>
   .back {
-    margin-left: -350px;
+    margin-left: -27%;
     position: fixed;
     left: 65%;
-    width: 300px;
+    width: 400px;
     margin-top: 5%;
-    height: calc(100% - 70%);
-    // overflow-y: hidden;
     display: flex;
     flex-direction: column;
     background: white;
-    // margin-top: 15px;
+    .login{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -51%;
+    background-color: white;
+    width: 400px;
+    height: 300px;
+    border-radius: 15px;
+    text-align: center;
+    padding: 5px 10px;
+    box-sizing: border-box;
     box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
-    // background: linear-gradient(to bottom, #66c6a1, #f2f2f2 50%);
+  }
+  p{
+    margin-top: 30px;
+    font-size: 42px;
+    font-weight: 600;
+}
     .form_back {
       margin-top: -5%;
       margin-left: 5%;
@@ -83,18 +99,25 @@
     }
     .title {
       margin-top: 15px;
-      margin-left: 45px;
+      margin-left: 60px;
       color: var(--themeColor);
       font-size: 13px;
       text-align: left;
     }
     .register_btn{
-      margin-top: -5%;
-      border-radius: 5px;
-      width: 80px;
-      height: 30px;
       margin-left: 32%;
-    }
+      width: 38%;
+      height: 48px;
+      border-radius: 8px;
+      margin-top: 10px;
+      font-size: 28px;
+      font-weight: 600;
+      color: white;
+      background-color: #42B285;
+      }
+      .register_btn:hover{
+      background-color: #59c2c5;
+  }
   }
   :deep(.van-nav-bar__title) {
     font-size: 20px;
